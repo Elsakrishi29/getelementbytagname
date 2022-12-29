c31c73514f4ccd96940b3c0125ad34e5ede63076
Krishnaveni <veniumabala@gmail.com>
2022-12-29 13:55:35 +0000

    add

---
+++ main.js
@@ -0,0 +1,9 @@
+/*var heading=document.getElementsByTagName("h3");
+function calc(){
+    alert("Total count : "+heading.length)
+}*/
+
+function show(){
+    var a=document.getElementsByTagName("h3");
+    alert(a[0].innerText);
+}
